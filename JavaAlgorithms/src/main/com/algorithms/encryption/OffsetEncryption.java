8ca2a9b40a40e6c2316d0d0f139298254fb16942
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-02-23 18:52:16 -0500

    Add files via upload

--- JavaAlgorithms/src/main/com/algorithms/encryption/OffsetEncryption.java
+++ JavaAlgorithms/src/main/com/algorithms/encryption/OffsetEncryption.java
@@ -1,5 +1,12 @@
 package main.com.algorithms.encryption;
 
+/**
+ * Methods to employ offset/caesar encryptions
+ * Historically a laughably weak encryption
+ * 
+ * @author Matthew Schofield
+ * @version 2.23.18
+ */
 public class OffsetEncryption {
 
 	public static String offsetEncrypt(String toEncrypt, int offSet)
@@ -20,7 +27,7 @@ public class OffsetEncryption {
 		return encryptedString;
 	}
 	
-	public static String progressiveOffsetEncryption(String toEncrypt, int offSet)
+	public static String progressiveOffsetEncrypt(String toEncrypt, int offSet)
 	{
 		String encryptedString = "";
 		char[] charArrayToEncrypt = toEncrypt.toCharArray();
@@ -30,10 +37,20 @@ public class OffsetEncryption {
 		return encryptedString;
 	}
 	
+	public static String progressiveOffsetDecrypt(String toDecrypt, int offSet)
+	{
+		String encryptedString = "";
+		char[] charArrayToEncrypt = toDecrypt.toCharArray();
+		for(int i = 0; i < charArrayToEncrypt.length; i++) {
+			encryptedString += (char) (charArrayToEncrypt[i] - i - offSet);
+		}
+		return encryptedString;
+	}
+	
 	public static void main(String[] args)
 	{
-		System.out.println(progressiveOffsetEncryption("aaa", 1));
-		System.out.println(progressiveOffsetEncryption("hiya", 0));
+		System.out.println(progressiveOffsetDecrypt(progressiveOffsetEncrypt("aaa", 1), 1));
+		System.out.println(progressiveOffsetEncrypt("hiya", 0));
 	}
 	
 }
