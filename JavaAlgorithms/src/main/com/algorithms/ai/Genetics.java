df9eba82e73baddb251e42e1b7e7fccdee348f52
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-02-28 09:55:33 -0500

    Add files via upload

--- JavaAlgorithms/src/main/com/algorithms/ai/Genetics.java
+++ JavaAlgorithms/src/main/com/algorithms/ai/Genetics.java
@@ -27,13 +27,39 @@ public class Genetics {
 
 		Random rand = new Random();
 		for(int i = 0; i < toMutate.length; i++) {
-			double mutation = -1 * range + rand.nextDouble() * (range - -1 * range);
-			toMutate[i] = toMutate[i] + mutation;
+			double mutation = -1.0 * range + (range - -1.0 * range) * rand.nextDouble();
+			toMutate[i] *= 1 + mutation;
 		}
 		return toMutate;
 	}
 	
+	public static Double[] swappingInheritance(Double[] parent1, Double[] parent2)
+	{
+		Double[] child = new Double[parent1.length];
+		Random rand = new Random();
+		for(int i = 0; i < parent1.length; i++) {
+			if(rand.nextBoolean()) {
+				child[i] = parent1[i];
+			}else {
+				child[i] = parent2[i];
+			}
+		}
+		return child;
+	}
+	
+	public static Double[] standardMutation(Double[] parent1, Double[] parent2)
+	{
+		Double[] child = swappingInheritance(parent1, parent2);
+		child = mutateByPercentage(child, 0.1);
+		return child;
+	}
+	
 	public static void main(String[] args)
 	{
+		Double[] parent1 = new Double[] {0.0, 1.0, 2.0, 3.0, 4.0};
+		Double[] parent2 = new Double[] {10.0, 9.0, 8.0, 7.0, 6.0};
+		Double[] child = standardMutation(parent1, parent2);
+		for(Double d : child)
+			System.out.println(d);
 	}
 }
