3d298ca798fe19bfbf46655ec4c9679433eb98d4
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-03-01 10:49:06 -0500

    Add files via upload

--- JavaAlgorithms/src/main/com/algorithms/filemanipulation/CreateFile.java
+++ JavaAlgorithms/src/main/com/algorithms/filemanipulation/CreateFile.java
@@ -3,25 +3,42 @@ package main.com.algorithms.filemanipulation;
 import java.io.IOException;
 import java.io.File;
 
+/**
+ *  Demonstrates how to create a File in java
+ * 
+ * @author Matthew Schofield
+ * @version 3.1.18
+ */
 public class CreateFile {
 
+	/**
+	 * Creates a File with the given name
+	 * 
+	 * @param fileName, name of the File to be created
+	 * @return if successfully created true is returned otherwise false
+	 */
 	public static boolean createFile(String fileName)
 	{
+		// Creates the necessary File object 
 		File file = new File(fileName);
+		
 		try {
+			// Creates the File in the system
 			file.createNewFile();
+			
+			// Prints out a message indicating success
 			System.out.println("File: " + fileName + " created");
+			
+			// True is returned as the File was successfully created
 			return true;
 		} catch (IOException e) {
-			System.out.println("IOException occurred: ");
-			e.printStackTrace();
+			//Indicates an IOException occurred
+			System.out.println("IOException occurred");
+			
+			// False is returned as the File was not created
 			return false;
 		}
 	}
 	
-	public static void main(String[] args)
-	{
-		createFile("Test.txt");
-	}
 	
 }
