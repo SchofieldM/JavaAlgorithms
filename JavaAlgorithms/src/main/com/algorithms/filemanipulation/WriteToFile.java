22bd3b7fd279b868299b3178e8db398a0c28a083
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-03-01 10:49:21 -0500

    Add files via upload

--- JavaAlgorithms/src/main/com/algorithms/filemanipulation/WriteToFile.java
+++ JavaAlgorithms/src/main/com/algorithms/filemanipulation/WriteToFile.java
@@ -4,24 +4,29 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Demonstrates how to write to a File in java
+ * 
+ * @author Matthew Schofield
+ * @version 3.1.18
+ */
 public class WriteToFile {
 
 	public static void writeToFile(String fileName, String informationToWrite)
 	{
 		try {
-		    String str = informationToWrite;
+			// A BufferedWriter is created targeting a File by the given fileName
 		    BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-		    writer.write(str);
-		     
+		    
+		    // Writes the String to the file
+		    writer.write(informationToWrite);
+		    
+		    // closes the writer
 		    writer.close();
 		} catch(IOException e) {
-			
+			// Indicates an IOException occurred
+			System.out.println("An IOException occurred");
 		}
-	}
-	
-	public static void main(String[] args)
-	{
-		writeToFile("Test.txt", "testing out \n the functionality \n\t of this");
 	}
 	
 }
