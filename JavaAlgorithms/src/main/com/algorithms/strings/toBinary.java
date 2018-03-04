99517c9a1c7aa49fe82d33f17dd243225423e9f7
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-03-03 21:54:14 -0500

    Add files via upload

--- JavaAlgorithms/src/main/com/algorithms/strings/toBinary.java
+++ JavaAlgorithms/src/main/com/algorithms/strings/toBinary.java
@@ -3,13 +3,20 @@ package main.com.algorithms.strings;
 /**
  * Allows for Strings to be converted to binary
  * 
- * "a" = 01100001
  * 
  * @author Matthew Schofield
- *
+ * @version 3.3.18
  */
 public class toBinary {
 
+	/**
+	 * Converts a String to a binary String
+	 * 
+	 * "a" = "01100001"
+	 * 
+	 * @param string, String to convert to a binary String
+	 * @return a binary String of the given String
+	 */
 	public static String toBinary(String string)
 	{
 		String textBinary = "";
@@ -23,6 +30,11 @@ public class toBinary {
 		return textBinary;
 	}
 	
+	/**
+	 * Demonstration of turning a String into a binary String
+	 * 
+	 * @param args, unused
+	 */
 	public static void main(String[] args)
 	{
 		System.out.println(toBinary("a"));
