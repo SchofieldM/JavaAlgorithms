bea6b8659b523b914bc8c9670a04cf32c8302e65
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-03-04 15:38:36 -0500

    Add files via upload

---
+++ JavaAlgorithms/src/main/com/algorithms/sounds/MakeDefaultSounds.java
@@ -0,0 +1,9 @@
+package main.com.algorithms.sounds;
+
+import java.awt.Toolkit;
+
+public class MakeDefaultSounds {
+	public static void main(String args[]) {
+		Toolkit.getDefaultToolkit().beep();
+    }
+}
