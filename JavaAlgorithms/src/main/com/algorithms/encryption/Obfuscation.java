b60a5a52f62a3b27ff816b8a0e205c5b33440a40
SchofieldM <36042254+SchofieldM@users.noreply.github.com>
2018-04-03 12:15:18 -0400

    Add files via upload

---
+++ JavaAlgorithms/src/main/com/algorithms/encryption/Obfuscation.java
@@ -0,0 +1,31 @@
+package main.com.algorithms.encryption;
+
+import main.com.algorithms.strings.BinaryToString;
+import main.com.algorithms.strings.toBinary;
+
+public class Obfuscation {
+
+	public static String flipBits(String inputString, int step)
+	{
+		String binaryInput = toBinary.toBinary(inputString);
+		System.out.println(binaryInput);
+		char[] binaryInputArr = binaryInput.toCharArray();
+		
+		for(int i = 0; i < binaryInputArr.length; i++) {
+			if(i != 0 && i%step == 0) {
+				char temp = binaryInputArr[i-1];
+				binaryInputArr[i-1] = binaryInputArr[i];
+				binaryInputArr[i] = temp;
+				System.out.println("Flip");
+			}
+		}
+		return BinaryToString.binaryToString8Bit(new String(binaryInputArr));
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(flipBits("aaa", 12));
+
+	}
+	
+}
